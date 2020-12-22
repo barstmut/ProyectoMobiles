3722d08c19cfc2c04cc2b3b48b8b3a500cdba9f3
Benjamin Parga <bparga17@alumnos.utalca.cl>
2020-12-22 18:14:24 -0300

    Cambios

---
+++ app/src/main/java/com/example/proyecto3/splash.kt
@@ -0,0 +1,15 @@
+package com.example.proyecto3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class splash : AppCompatActivity()
+{
+
+    
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+    }
+}
