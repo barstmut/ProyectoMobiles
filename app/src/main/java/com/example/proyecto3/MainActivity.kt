9a07b7aabfa0d3c127d40fd0c07161d494fa8e4d
Syncro99 <43821515+Syncro99@users.noreply.github.com>
2020-12-23 17:29:25 -0300

    Add files via upload

--- app/src/main/java/com/example/proyecto3/MainActivity.kt
+++ app/src/main/java/com/example/proyecto3/MainActivity.kt
@@ -3,21 +3,34 @@ package com.example.proyecto3
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity()
 {
+    lateinit var blink: Animation
+    lateinit var fadeout: Animation
+
     override fun onCreate(savedInstanceState: Bundle?) 
     {
+        blink= AnimationUtils.loadAnimation(this,R.anim.blink)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         val saveData = SaveData(applicationContext)
         saveData.setAlarm()
+
+        setContentView(R.layout.activity_main)
+        tv_push.startAnimation(blink)
     }
 
-    fun change()
+    fun change(view: View)
     {
-        val intent = Intent(this, category::class.java)
+        fadeout= AnimationUtils.loadAnimation(this,R.anim.fade_out)
+        tv_push.startAnimation(fadeout)
+        val intent: Intent = Intent(this, category::class.java)
         startActivity(intent)
     }
 
